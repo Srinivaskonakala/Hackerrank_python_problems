bf7faaea7c1ec62e44da9d6bb5ccab016caf9a64
Srinivas <147386887+ksrinivas07@users.noreply.github.com>
2026-09-18 10:46:22 +0530

    lambda_fibonacci

---
+++ lambda_fibonacci.py
@@ -0,0 +1,12 @@
+cube=lambda x: x**3
+def fibonacci(n):
+    res=[]
+    a,b=0,1
+    for i in range(n):
+        res.append(a)
+        a,b=b,a+b
+    return res
+
+if __name__ == '__main__':
+    n = int(input())
+    print(list(map(cube, fibonacci(n))))
