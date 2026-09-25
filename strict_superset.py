67e80ab62a89a985b171e887ffc03920dffd199a
Srinivas <147386887+ksrinivas07@users.noreply.github.com>
2026-09-25 10:33:14 +0530

    strict superset problem

--- strict_superset.py
+++ strict_superset.py
@@ -1,5 +1,9 @@
 a=set(map(int,input().split()))
 n=int(input())
+result=True
 for i in range(n):
     set_n=set(map(int,input().split()))
-print(a.issuperset(set_n))
+    if not a.issuperset(set_n):
+        result=False
+    
+print(result)
