15541b0136d0951357d52c380a706b41d886408d
Srinivas <147386887+ksrinivas07@users.noreply.github.com>
2026-09-25 10:12:53 +0530

    checking a is subset of b

---
+++ subset_problem.py
@@ -0,0 +1,7 @@
+t=int(input())
+for i in range(t):
+    len_a=int(input())
+    a=set(map(int,input().split()))
+    len_b=int(input())
+    b=set(map(int,input().split()))
+    print(a.issubset(b))
